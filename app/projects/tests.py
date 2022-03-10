84858fc4943fe78263bc6dbe868da865ee2ea606
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2022-03-10 22:33:18 +0100

    Write tests the project share should fulfill

--- app/projects/tests.py
+++ app/projects/tests.py
@@ -1,12 +1,14 @@
+import pytest
 from django.test import TestCase
 from django.urls import reverse
 from django.conf import settings as django_settings
 from django.test.client import RequestFactory
-from projects.models import Project
+from projects.models import Project, Viewer
+from users.models import CustomUser
 
 
 class BasicOperationsTest(TestCase):
-    fixtures = ["fixtures/benchmarks_fixture.json"]
+    fixtures = ["fixtures/benchmarks_fixture.json", "fixtures/test_users.json"]
 
     @classmethod
     def setUpTestData(cls):
@@ -15,6 +17,7 @@ class BasicOperationsTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.client.login(username="testUser", password="ASas12,.")
+        self.project = Project.objects.get(id=1)
 
     def test_delete_project_redirects(self):
         """Make sure we are redirected to project page once deleting a project"""
@@ -31,3 +34,53 @@ class BasicOperationsTest(TestCase):
         response = self.client.get(response.url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(Project.objects.all().count(), 2)
+
+    def test_add_new_viewer_to_project(self):
+        test_email = CustomUser.objects.last().email
+        success, _ = self.project.add_viewer_if_not_exist(
+            email=test_email, share_rights="edit"
+        )
+        self.assertTrue(success)
+        self.assertTrue(self.project.viewers.filter(user__email=test_email).exists())
+
+    def test_add_existing_viewer_to_project(self):
+        test_email = CustomUser.objects.last().email
+        self.project.add_viewer_if_not_exist(email=test_email, share_rights="edit")
+
+        success, _ = self.project.add_viewer_if_not_exist(
+            email=test_email, share_rights="edit"
+        )
+        self.assertFalse(success)
+        self.assertEqual(self.project.viewers.filter(user__email=test_email).count(), 1)
+
+    def test_update_viewer_rights_to_project(self):
+        test_email = CustomUser.objects.last().email
+        self.project.add_viewer_if_not_exist(email=test_email, share_rights="edit")
+
+        success, _ = self.project.add_viewer_if_not_exist(
+            email=test_email, share_rights="read"
+        )
+        self.assertTrue(success)
+        self.assertEqual(
+            self.project.viewers.get(user__email=test_email).share_rights, "read"
+        )
+        self.assertEqual(self.project.viewers.filter(user__email=test_email).count(), 1)
+
+    def test_add_project_user_as_viewer(self):
+        test_email = CustomUser.objects.first().email
+        success, _ = self.project.add_viewer_if_not_exist(
+            email=test_email, share_rights="edit"
+        )
+        self.assertFalse(success)
+        self.assertFalse(self.project.viewers.filter(user__email=test_email).exists())
+
+    def test_add_project_viewer_via_post(self):
+        test_email = CustomUser.objects.last().email
+        response = self.client.post(
+            reverse("project_share", args=[1]),
+            dict(email=test_email, share_rights="read"),
+        )
+        self.assertRedirects(response, reverse("project_search", args=[1]))
+        response = self.client.get(response.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.project.viewers.filter(user__email=test_email).count(), 1)

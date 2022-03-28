d7be2d4b11a5cc4a635fdfaafd0f960f0fcf4fc9
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2022-03-28 15:00:42 +0200

    Add Sankey diagram forms

--- app/dashboard/helpers.py
+++ app/dashboard/helpers.py
@@ -500,5 +500,12 @@ GRAPH_PARAMETERS_SCHEMAS = {
     GRAPH_BAR: {},
     GRAPH_PIE: {},
     GRAPH_LOAD_DURATION: {},
-    GRAPH_SANKEY: {},
+    GRAPH_SANKEY: {
+        "type": "object",
+        "required": ["energy_vector"],
+        "properties": {
+            "energy_vector": {"oneOf": [{"type": "array", "items": {"type": "string"}}]}
+        },
+        "additionalProperties": False,
+    },
 }

65f84fd447e8665cc762b4a87956e45fed661e93
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2022-04-14 10:02:06 +0200

    Format form with errors before returning it
    This is for javascript

--- app/projects/scenario_topology_helpers.py
+++ app/projects/scenario_topology_helpers.py
@@ -46,8 +46,11 @@ def handle_bus_form_post(request, scen_id=0, asset_type_name="", asset_uuid=None
         bus.save()
         return JsonResponse({"success": True, "asset_id": bus.id}, status=200)
     logger.warning(f"The submitted bus has erroneous field values.")
-    form_html = crispy_forms_filters.as_crispy_form(form)
-    return JsonResponse({"success": False, "form_html": form_html}, status=422)
+
+    form_html = get_template("asset/bus_create_form.html")
+    return JsonResponse(
+        {"success": False, "form_html": form_html.render({"form": form})}, status=422
+    )
 
 
 def handle_storage_unit_form_post(
@@ -163,8 +166,11 @@ def handle_asset_form_post(request, scen_id=0, asset_type_name="", asset_uuid=No
         asset.save()
         return JsonResponse({"success": True, "asset_id": asset.unique_id}, status=200)
     logger.warning(f"The submitted asset has erroneous field values.")
-    form_html = crispy_forms_filters.as_crispy_form(form)
-    return JsonResponse({"success": False, "form_html": form_html}, status=422)
+
+    form_html = get_template("asset/asset_create_form.html")
+    return JsonResponse(
+        {"success": False, "form_html": form_html.render({"form": form})}, status=422
+    )
 
 
 def load_scenario_topology_from_db(scen_id):

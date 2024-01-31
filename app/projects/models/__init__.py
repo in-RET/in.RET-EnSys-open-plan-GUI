99fe6f5748804d486dd33207df71e841762b3ed6
Andreas Lubojanski <andreas@lubojanski.com>
2024-01-31 16:50:27 +0100

    modified docker compose file for production
    Signed-off-by: Andreas Lubojanski <andreas@lubojanski.com>

--- app/projects/models/__init__.py
+++ app/projects/models/__init__.py
@@ -1,1 +1,7 @@
 from projects.models.base_models import *
+from projects.models.simulation_models import (
+    Simulation,
+    SensitivityAnalysis,
+    get_project_sensitivity_analysis,
+)
+from projects.models.usecases import UseCase, load_usecase_from_dict
